12adde8d0c7d3d6a845a53a471fc9f1395bb6e2c
tomoya1031 <rowingwant@yahoo.co.jp>
2021-01-28 23:24:23 +0900

    wcp-4

--- JavaWcp/src/Chapter02.java
+++ JavaWcp/src/Chapter02.java
@@ -1,6 +1,7 @@
 
 public class Chapter02 {
     public static void main(String[] args) {
-        System.out.println("こんにちは!");
+    	System.out.println("Hello WEBCANP!");
+        System.out.println("Hello JAVA!");
     }
 }
---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,15 @@
+
+public class Chapter04 {
+
+	public static void main(String[] args) {
+
+		String name;
+		name = "tomoya";
+		name = "itigo";
+		System.out.println(name);
+		final int LUCKY_NUMBER;
+		LUCKY_NUMBER = 7;
+		System.out.println(LUCKY_NUMBER);
+	}
+
+}
